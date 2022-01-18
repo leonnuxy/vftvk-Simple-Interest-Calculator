7f0f502b25554653af1738b295ca7bf14e5b1370
Noel <1leonnoel1@gmail.com>
2022-01-18 14:12:51 -0700

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * rate * years) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+}
+
+function updateRate()
+{
+    var reteval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = reteval;
     
 }
         
